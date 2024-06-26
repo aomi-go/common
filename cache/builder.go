ab8d34921676fd69368493df43c2a0bd81c3d109
sean <xxoo@make.love>
2024-06-26 17:19:01 +0800

    feat: cache helper

---
+++ cache/builder.go
@@ -0,0 +1,46 @@
+package cache
+
+import "context"
+
+// NewHelper creates a new Helper.
+func NewHelper(c Cache) Helper {
+	return Helper{
+		c: c,
+	}
+}
+
+type Helper struct {
+	c      Cache
+	ctx    context.Context
+	entry  Entry
+	suffix string
+	v      any
+}
+
+func (b Helper) WithContext(ctx context.Context) Helper {
+	b.ctx = ctx
+	return b
+}
+
+func (b Helper) WithEntry(entry Entry) Helper {
+	b.entry = entry
+	if "" != b.suffix {
+		b.entry = b.entry.SetKeySuffix(b.suffix)
+	}
+	return b
+}
+
+func (b Helper) WithSuffix(suffix string) Helper {
+	b.suffix = suffix
+	b.entry = b.entry.SetKeySuffix(suffix)
+	return b
+}
+
+func (b Helper) Value(v any) Helper {
+	b.v = v
+	return b
+}
+
+func (b Helper) Set() error {
+	return b.c.Set(b.ctx, b.entry.Key, b.v, b.entry.Ttl)
+}
