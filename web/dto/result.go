6a0f5c148368bb11c26a7dff2946d9e8b28fa71f
sean <xxoo@make.love>
2025-08-07 14:16:51 +0800

    feat: 使用泛型

--- web/dto/result.go
+++ web/dto/result.go
@@ -1,7 +1,7 @@
 package dto
 
-type Result struct {
+type Result[T any] struct {
 	Status   string `json:"status" bson:"status" description:"状态码" example:"0000"`
 	Describe string `json:"describe,omitempty" bson:"describe,omitempty" description:"描述" example:"成功"`
-	Payload  any    `json:"payload,omitempty" bson:"payload,omitempty" description:"数据" example:"{}"`
+	Payload  T      `json:"payload,omitempty" bson:"payload,omitempty" description:"数据" example:"{}"`
 }
