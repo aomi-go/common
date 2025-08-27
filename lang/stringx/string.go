3e61f9111c8f9c1a4386e12e639b0a09746c8e7e
sean <xxoo@make.love>
2025-08-27 09:23:09 +0800

    feat: Is empty

--- lang/stringx/string.go
+++ lang/stringx/string.go
@@ -5,3 +5,11 @@ import "strings"
 func EqualsIgnoreCase(a, b string) bool {
 	return strings.ToLower(a) == strings.ToLower(b)
 }
+
+func IsEmpty(str string) bool {
+	return len(str) == 0 || strings.Trim(str, " ") == ""
+}
+
+func IsNotEmpty(str string) bool {
+	return !IsEmpty(str)
+}
