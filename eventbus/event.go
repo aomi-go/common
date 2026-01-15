9e650d7fa62fb6408fe396e2714451ccef829a38
sean <xxoo@make.love>
2026-01-15 12:49:21 +0800

    feat: base eventeventbus

--- eventbus/event.go
+++ eventbus/event.go
@@ -6,3 +6,16 @@ type Event interface {
 	Name() string
 	Time() time.Time
 }
+
+type BaseEvent[T interface{}] struct {
+	EventName string
+	EventAt   time.Time
+	Data      T
+}
+
+func (e *BaseEvent[T]) Name() string {
+	return e.EventName
+}
+func (e *BaseEvent[T]) Time() time.Time {
+	return e.EventAt
+}
